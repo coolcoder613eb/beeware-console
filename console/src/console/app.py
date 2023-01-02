38415630f8b33030f6ded8a42e039eebf9470353
coolcoder613eb <96163908+coolcoder613eb@users.noreply.github.com>
2023-01-02 19:16:04 +1100

    Update app.py

--- console/src/console/app.py
+++ console/src/console/app.py
@@ -7,7 +7,8 @@ from toga.style.pack import COLUMN, LEFT, RIGHT, CENTER, ROW, Pack, TOP, BOTTOM
 
 
 class Console(toga.App):
-
+    def ok(self,button):
+        print(self.input.value)
     def startup(self):
         """
         Construct and show the Toga application.
@@ -16,16 +17,20 @@ class Console(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
-        text = [[' ' for y in range(80)] for x in range(25)]
-        main_box = toga.Box()
-        main_box.style.update(direction=COLUMN, padding=0)
-        self.word = toga.Label("\n".join("".join(x) for x in text))
-        word_box = toga.Box()
-        main_box.add(self.word)
+        self.text = ''#(('█'*80)+'\n')*25
+        self.main_box = toga.Box()
+        self.main_box.style.update(direction=COLUMN, padding=0)
+        self.word = toga.Label("\n".join("".join(x) for x in self.text))
+        self.word_box = toga.Box()
+        self.main_box.style.update(direction=ROW, padding=0)
+        self.main_box.add(self.word)
+        self.main_box.add(self.word_box)
+        self.input = toga.TextInput()
+        self.word_box.add(self.input)
 
 
         self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
+        self.main_window.content = self.main_box
         self.main_window.show()
 
 
